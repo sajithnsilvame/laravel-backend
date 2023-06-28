fcf02c1e241c84b9e83efa7c4a7da4421687f565
Kasun Anuradha <anuradhakasun12345@gmail.com>
2023-06-28 22:29:52 +0530

    order create

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Stripe\Stripe;
+use Stripe\PaymentIntent;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Stripe\Exception\CardException;
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 
 class PaymentController extends Controller
 {
-    public function make_payment(Request $request){
-        
-        
+    public function make_payment(Request $request)
+    {
+
+        $data = $request->all();
         $amount = $request->input('amount');
         $apiKey = env('STRIPE_SECRET');
 
@@ -25,8 +30,37 @@ class PaymentController extends Controller
             'description' => 'Payment from customer',
         ]);
 
+        //order create
+        //order user info
+        $userOrder = [
+            'cus_name' => $data['order_info']['user'][0]['fname'] . " " . $data['order_info']['user'][0]['lname'],
+            'mobile' => $data['order_info']['user'][0]['mobile'],
+            'email' => $data['order_info']['user'][0]['email'],
+            'address' => $data['order_info']['user'][0]['address'],
+            'city' => $data['order_info']['user'][0]['city'],
+        ];
+        //order item info
+        foreach ($data['order_info']['order'] as $post) {
+            //order user create
+            $orderCreate =  Order::create($userOrder);
+            $orderItem = [
+                'title' => $post['title'],
+                'image' => $post['image'],
+                'qty' => $post['quantity'],
+                'price' => $post['price'],
+                'size' =>    $post['size'],
+                'color' => $post['color'],
+                'total' => $post['price'],
+            ];
+            //order item create
+            $orderCreate->update($orderItem);
+        }
+
+
         return response()->json([
             'client_secret' => $intent->client_secret,
         ]);
     }
 }
+
+
